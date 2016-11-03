7ef0958e3ecef9d0d4efa59cee39bc26439b5c00
Javi Sebastia <fjsebastiam@gmail.com>
2016-11-03 21:10:01 +0100

    commit con funcion

--- src/hello_world.java
+++ src/hello_world.java
@@ -3,11 +3,7 @@
  */
 public class hello_world {
     public static void main(String[] args) {
-        public static void main(String[] args) {
-            printHola();
-        }
-        public static void printHola(){
-            System.out.println("Hola món!");
-        }
+        f_print aa =new f_print();
+        aa.printHola();
     }
 }
